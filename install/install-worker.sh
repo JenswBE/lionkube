7a7c74072fe165dd3b1897b7ade30ea7faa23fe6
JenswBE <jens@jensw.be>
2019-12-11 21:48:57 +0100

    Set cloud provider to External on workers

--- install/install-worker.sh
+++ install/install-worker.sh
@@ -9,6 +9,11 @@ FL_IP=<FLOATING_IP>
 sudo ufw allow in on ${INT_IF} to any port 10250 proto tcp # Kubelet
 sudo ufw allow in on ${INT_IF} to any port 8472  proto udp # Canal
 
+# Set cloud provider to external
+cat <<EOF | sudo tee /etc/default/kubelet
+KUBELET_EXTRA_ARGS=--cloud-provider=external
+EOF
+
 # Cluster worker join
 sudo kubeadm join cluster-endpoint:6443 --token ilcokw.423bkb8y4yll10c2 \
     --discovery-token-ca-cert-hash sha256:8dc950495ea73ed02bc79e1de53fefb8fcb035c6727577d679184df191632d3d
