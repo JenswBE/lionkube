bf643c58158e30643acac376fac13497e1394b2e
JenswBE <jens@jensw.be>
2019-12-11 23:33:44 +0100

    Rename FL_IP to HETZNER_FLOATING_IP

--- install/install-worker.sh
+++ install/install-worker.sh
@@ -3,7 +3,7 @@
 # Config
 EXT_IF=eth0
 INT_IF=ens10
-FL_IP=<FLOATING_IP>
+HETZNER_FLOATING_IP=REPLACE_ME
 
 # Firewall
 sudo ufw allow in on ${INT_IF} to any port 10250 proto tcp # Kubelet
@@ -21,6 +21,6 @@ sudo kubeadm join cluster-endpoint:6443 --token ilcokw.423bkb8y4yll10c2 \
 # Floating IP
 echo -e "auto ${EXT_IF}:1
 iface ${EXT_IF}:1 inet static
-  address ${FL_IP}
+  address ${HETZNER_FLOATING_IP}
   netmask 32" | sudo tee /etc/network/interfaces.d/60-floating-ip.cfg
 sudo systemctl restart networking
