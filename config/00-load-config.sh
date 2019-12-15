db2ad190760dc65544d5ee816d61718ae94bed52
Jens Willemsens <jens@jensw.be>
2019-12-15 21:41:37 +0100

    Make less verbose

--- config/00-load-config.sh
+++ config/00-load-config.sh
@@ -5,15 +5,15 @@ CONFIG_DIR="${BASH_SOURCE%/*}"
 
 # Load general config
 CONF_GEN=${CONFIG_DIR}/10-general.sh
-echo "Loading config from ${CONF_GEN} ..."
+echo "Loading ${CONF_GEN} ..."
 source "${CONF_GEN}"
 
 # Load components config
 CONF_COMP=${CONFIG_DIR}/20-components.sh
-echo "Loading config from ${CONF_COMP} ..."
+echo "Loading ${CONF_COMP} ..."
 source "${CONF_COMP}"
 
 # Load services config
 CONF_SVC=${CONFIG_DIR}/30-services.sh
-echo "Loading config from ${CONF_SVC} ..."
+echo "Loading ${CONF_SVC} ..."
 source "${CONF_SVC}"
