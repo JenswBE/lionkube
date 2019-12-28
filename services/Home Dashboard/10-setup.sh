9c2b6ff8c55f912b66e7364ea6203d22cc20f584
Jens Willemsens <jens@jensw.be>
2019-12-28 12:03:13 +0100

    Home Dashboard: Add InfluxDB configuration in setup script

--- services/Home Dashboard/10-setup.sh
+++ services/Home Dashboard/10-setup.sh
@@ -39,3 +39,40 @@ kubectl create configmap \
 ../../kube-apply-env ./50-grafana.yml
 ../../kube-apply-env ./60-ingress-influxdb.yml
 ../../kube-apply-env ./61-ingress-grafana.yml
+
+# Configure InfluxDB
+set +o history # Disable history
+TELEGRAF_PASS=REPLACE_ME
+GRAFANA_PASS=REPLACE_ME
+set +o history # Enable history
+
+INFLUXDB_POD=$(kubectl get pod -n home-dashboard --selector=app=influxdb -o jsonpath="{.items[0].metadata.name}")
+cat <<EOF | kubectl exec -n home-dashboard -i ${INFLUXDB_POD:?} -- \
+  influx -username "${HOME_DASHBOARD_INFLUXDB_ADMIN_USER:?}" -password "${HOME_DASHBOARD_INFLUXDB_ADMIN_PASS:?}"
+-- Create database
+CREATE DATABASE "monitor_temp" WITH DURATION 1w NAME "one_week";
+
+-- Create retention period for downsampled data
+CREATE RETENTION POLICY "infinite" ON "monitor_temp" DURATION INF REPLICATION 1;
+
+-- Create a continuous query to downsample the data
+CREATE CONTINUOUS QUERY "aggr_5min" ON "monitor_temp" BEGIN \
+  SELECT mean("temperature") AS "mean_temperature",mean("humidity") AS "mean_humidity", mean("heat-index") AS "mean_heat-index" \
+  INTO "infinite"."downsampled_temp" \
+  FROM "monitor-temp" \
+  GROUP BY time(5m) \
+END;
+
+-- Create a user for Telegraf to feed the data
+CREATE USER "telegraf_monitor_temp" WITH PASSWORD '${TELEGRAF_PASS}';
+
+-- Grant write rights to Telegraf
+GRANT WRITE ON "monitor_temp" TO "telegraf_monitor_temp";
+
+-- Create a user for Grafana to display the data
+CREATE USER "grafana" WITH PASSWORD '${GRAFANA_PASS}';
+
+-- Grant read rights to Grafana
+GRANT READ ON "monitor_temp" TO "grafana";
+
+EOF
