0393ab748e29e71063c1660797d0a9a5e1e0ed3d
Jens Willemsens <jens@jensw.be>
2019-12-29 23:12:56 +0100

    Nextcloud: Fix reverse proxy handling

--- services/Nextcloud/11-setup-nextcloud.sh
+++ services/Nextcloud/11-setup-nextcloud.sh
@@ -3,9 +3,9 @@
 # Load config
 source ../../config/00-load-config.sh
 
-# =============================
-# = EXECUTE ON MASTER NODE(S) =
-# =============================
+# =====================================
+# =     EXECUTE ON MASTER NODE(S)     =
+# =====================================
 
 # Label nodes (repeat for each node you executed above instructions on)
 kubectl label nodes <NODE_NAME> mount.media.nextcloud=true
@@ -28,3 +28,35 @@ kubectl create secret generic \
 ../../kube-apply-env ./40-mariadb.yml
 ../../kube-apply-env ./50-nextcloud.yml
 ../../kube-apply-env ./60-ingress.yml
+
+# Check which host is running the Nextcloud pod
+kubectl get pod -n nextcloud --selector=app=nextcloud -o wide
+
+# =====================================
+# = EXECUTE ON NODE RUNNING NEXTCLOUD =
+# =====================================
+# Currently kubectl doesn't support running exec as another user.
+# Therefore, you have to execute below commands directly on the
+# worker node.
+# See https://github.com/kubernetes/kubernetes/issues/30656 for more info
+
+# Fix reverse proxy handling
+NC_CONTAINER=$(docker ps | grep nextcloud_nextcloud | cut -f1 -d" ")
+docker exec -u www-data ${NC_CONTAINER} php occ config:system:set overwriteprotocol --value="https"
+docker exec -u www-data ${NC_CONTAINER} php occ config:system:set trusted_proxies 0 --value="172.16.0.0/12"
+
+# =====================================
+# =        CONFIGURE NEXTCLOUD        =
+# =====================================
+# Disable following apps:
+# - First run wizard
+#
+# Install and configure following apps:
+# - Contacts
+# - Calendar
+# - Tasks
+# - Notes
+# - Group folders
+# - Quota warning
+# - Preview generator
+# - AppOrder
