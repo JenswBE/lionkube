5f70629b7287292e4887e1359a83155bed40346a
Jens Willemsens <jens@jensw.be>
2019-12-31 16:13:10 +0100

    Add service Passit

---
+++ services/Passit/10-setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# =============================
+# = EXECUTE ON MASTER NODE(S) =
+# =============================
+
+# Load config
+source ../../config/00-load-config.sh
+
+# Init Passit
+kubectl apply -f ./20-namespace.yml
+kubectl apply -f ./30-storage.yml
+
+# Create config
+kubectl create secret generic \
+  -n passit \
+  postgresql-password \
+  --from-literal=POSTGRES_PASSWORD=${PASSIT_DB_PASS:?}
+kubectl create secret generic \
+  -n passit \
+  passit-secret-key \
+  --from-literal=SECRET_KEY=${PASSIT_SECRET_KEY:?}
+kubectl create secret generic \
+  -n passit \
+  passit-email-url \
+  --from-literal=EMAIL_URL="submission://${PASSIT_MAIL_USER:?}:${PASSIT_MAIL_PASS:?}@${PASSIT_MAIL_HOST:?}"
+
+
+# Deploy Passit
+../../kube-apply-env ./40-postgresql.yml
+../../kube-apply-env ./50-passit.yml
+../../kube-apply-env ./60-ingress.yml
