fa59e5bd7bec9a72faba3a9a15f668493d882389
Jens Willemsens <jens@jensw.be>
2020-01-07 22:11:44 +0100

    Passit: Merge backup into setup.sh

--- services/Passit/10-setup.sh
+++ services/Passit/10-setup.sh
@@ -24,8 +24,14 @@ kubectl create secret generic \
   -n passit \
   passit-email-url \
   --from-literal=EMAIL_URL="smtp+tls://${PASSIT_MAIL_USER:?}:${PASSIT_MAIL_PASS:?}@${PASSIT_MAIL_HOST:?}:587"
+kubectl create secret generic \
+  -n passit \
+  backup-postgresql-pgpass \
+  --from-literal=pgpass=postgresql:5432:passit:passit:${PASSIT_DB_PASS:?}
+  # hostname:port:database:username:password
 
 # Deploy Passit
 ../../kube-apply-env ./40-postgresql.yml
 ../../kube-apply-env ./50-passit.yml
 ../../kube-apply-env ./60-ingress.yml
+kubectl apply -f ./70-backup-postgresql.yml
