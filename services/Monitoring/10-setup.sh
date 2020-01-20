b01efe66f02e24451cdd379b04f160d823466b3a
Jens Willemsens <jens@jensw.be>
2020-01-20 11:39:19 +0100

    Monitoring: Bump Prometheus chart to v10.3.1

--- services/Monitoring/10-setup.sh
+++ services/Monitoring/10-setup.sh
@@ -16,4 +16,4 @@ sudo ufw allow in on ${INT_IF:?} to any port 9100 proto tcp # Node exporter
 
 # Install Prometheus
 kubectl apply -f ./20-namespace.yml
-helm install prometheus stable/prometheus --version 9.7.4 -n monitoring -f ./30-values-prometheus.yml
+helm install prometheus stable/prometheus --version 10.3.1 -n monitoring -f ./30-values-prometheus.yml
